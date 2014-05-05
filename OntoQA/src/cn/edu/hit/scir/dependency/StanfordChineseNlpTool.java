b20c2a2940fdb140bc0f46edc45df670962dabb1
spkang <spkangirhit@gmail.com>
2014-05-05 12:50:27 +0800

    add a chinese nlp tool file

---
+++ OntoQA/src/cn/edu/hit/scir/dependency/StanfordChineseNlpTool.java
@@ -0,0 +1,20 @@
+/**
+ * Copyright 2014 HIT-SCIR
+ * Research Center for Information Retrieval and Social Network
+ * Harbin Institute of Technology
+ * http://ir.hit.edu.cn
+ */
+package cn.edu.hit.scir.dependency;
+
+/**
+ *   句法分析及分词
+ * 
+ *
+ * @author spkang (kangshupeng@163.com)
+ * @version 0.1.0
+ * @date 2014年5月4日 
+ */
+public class StanfordChineseNlpTool {//implements StanfordNlpTool {
+	private static StanfordChineseNlpTool instance;
+	
+}
