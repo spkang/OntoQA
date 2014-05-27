32f83bc7d1268c0d7c0eb44535dfb4270c9db10e
spkang <spkangirhit@gmail.com>
2014-05-27 14:28:36 +0800

    add some test

---
+++ OntoQA/test/cn/edu/hit/scir/ProbabilityGraph/ProbabilityGraphAnalyzerTest.java
@@ -0,0 +1,26 @@
+/**
+ * Copyright 2014 HIT-SCIR
+ * Research Center for Information Retrieval and Social Network
+ * Harbin Institute of Technology
+ * http://ir.hit.edu.cn
+ */
+package cn.edu.hit.scir.ProbabilityGraph;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ *
+ * @author spkang (kangshupeng@163.com)
+ * @version 0.1.0
+ * @date 2014年5月27日 
+ */
+public class ProbabilityGraphAnalyzerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
